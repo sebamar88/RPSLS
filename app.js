2090834ca3151c1a42362146b1467e28cd5223e2
Sebastián Martinez <sebamar88@gmail.com>
2021-07-03 16:27:09 -0300

    glow

--- app.js
+++ app.js
@@ -48,16 +48,22 @@ function generateComputerChoice() {
 function getResult() {
     if (computerChoice === userChoice) {
       result = "It's a draw!"
+      userChoiceDisplay.style.boxShadow = 'none'
+      userChoiceDisplay.style.borderRadius = '50%'
       drawCounter += 1;
       drawsCounterDisplay.innerHTML = `You drew ${drawCounter} times`
     }
     if (computerChoice === 'rock' && userChoice === "paper") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'rock' && userChoice === "spock") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
@@ -73,81 +79,113 @@ function getResult() {
     }
     if (computerChoice === 'paper' && userChoice === "scissors") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'paper' && userChoice === "lizzard") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'paper' && userChoice === "rock") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'paper' && userChoice === "spock") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'scissors' && userChoice === "rock") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'scissors' && userChoice === "spock") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'scissors' && userChoice === "paper") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'scissors' && userChoice === "lizzard") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'spock' && userChoice === "lizzard") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'spock' && userChoice === "paper") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'spock' && userChoice === "scissors") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'spock' && userChoice === "rock") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'lizzard' && userChoice === "scissors") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'lizzard' && userChoice === "rock") {
       result = 'You win!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #1dea37'
+      userChoiceDisplay.style.borderRadius = '50%'
       winsCounter += 1;
       winsCounterDisplay.innerHTML = `You won ${winsCounter} times`
     }
     if (computerChoice === 'lizzard' && userChoice === "paper") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
     if (computerChoice === 'lizzard' && userChoice === "spock") {
       result = 'You lose!'
+      userChoiceDisplay.style.boxShadow = '0px 0px 50px 10px #ea3131'
+      userChoiceDisplay.style.borderRadius = '50%'
       loseCounter += 1;
       loseCounterDisplay.innerHTML = `You lost ${loseCounter} times`
     }
