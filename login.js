6f6ba187fe27424188067545c68a15a8d56952ca
Sebastián Martinez <sebamar88@gmail.com>
2021-07-28 19:51:54 -0300

    added toasts to Register Form

--- login.js
+++ login.js
@@ -1,131 +1,169 @@
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
+// Style for Logged and not
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
 
+// Register Toast
+const errorToastR = document.querySelector(".toastR");
+const errorMsgR = document.querySelector(".toastR-body");
+const toasCloseR = document.querySelector("#toastCloseR");
+
+// Login Toast
+const errorToastL = document.querySelector(".toastL");
+const errorMsgL = document.querySelector(".toast-bodyL");
+const toasCloseL = document.querySelector("#toastCloseL");
+
+//Login Form
+const loginForm = document.querySelector("#loginForm");
+
+// Modals Login / Register
+const signupModal = new bootstrap.Modal(document.getElementById("signupModal"));
+const signinModal = new bootstrap.Modal(document.getElementById("loginModal"));
+
+//Sign up Form
+const signupForm = document.querySelector("#signupForm");
+
+// Logout button
+const logoutButton = document.querySelector("#logout");
+const googleButton = document.querySelector("#googleButton");
+
+// Functions 
 const loginCheck = (user) => {
-  if(user){      
-      loggedInLinks.forEach(link => link.style.display = 'block')
-      loggedOutLinks.forEach(link => link.style.display = 'none')
-  } else {
-      loggedInLinks.forEach(link => link.style.display = 'none')
-      loggedOutLinks.forEach(link => link.style.display = 'block')
-  }
+    if (user) {
+        loggedInLinks.forEach((link) => (link.style.display = "block"));
+        loggedOutLinks.forEach((link) => (link.style.display = "none"));
+    } else {
+        loggedInLinks.forEach((link) => (link.style.display = "none"));
+        loggedOutLinks.forEach((link) => (link.style.display = "block"));
+    }
+};
+
+
+const showUser = () => {
+    const actualUser = document.querySelector("#userLogged");
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+    actualUser.innerHTML = `Welcome ${currentUser}`;
+};
+
+if (localStorage.getItem("user") != null) {
+    showUser();
 }
 
-//Sign up
-const signupForm = document.querySelector('#signupForm');
-
-signupForm.addEventListener('submit', (e)=>{
+// Events
+signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const signupEmail = document.querySelector('#signupEmail').value
-    const signupPassword = document.querySelector('#signupPassword').value
+    const signupEmail = document.querySelector("#signupEmail").value;
+    const signupPassword = document.querySelector("#signupPassword").value;
 
     auth
         .createUserWithEmailAndPassword(signupEmail, signupPassword)
-        .then(()=>{
+        .then(() => {
             //clear form
             signupForm.reset();
             signupModal.hide();
-            console.log('Registrado de forma exitosa')
+            console.log("Registrado de forma exitosa");
         })
-})
+        .catch((error) => {
+            switch (error.code) {
+                case "auth/weak-password":
+                    errorToastR.classList.toggle("show");
+                    errorMsgR.innerHTML = "Password should be at least 6 characters";
+                    break;
+                case "auth/email-already-in-use":
+                    errorToastR.classList.toggle("show");
+                    errorMsgR.innerHTML =
+                        "The email address is already in use by another account.";
+                    break;
+                default:
+                    break;
+            }
+        });
+});
 
-//Login
-const loginForm = document.querySelector('#loginForm');
-const signupModal = new bootstrap.Modal(document.getElementById('signupModal'));
-const signinModal = new bootstrap.Modal(document.getElementById('loginModal'));
-const errorToast = document.querySelector('.toast')
-const errorMsg = document.querySelector('.toast-body')
-const toasClose = document.querySelector('#toastClose')
-const myToast = bootstrap.Toast.getInstance(errorToast)
+toasCloseR.addEventListener("click", () => {
+    errorToastR.classList.toggle("show");
+});
 
-const showUser = () => {
-  const actualUser = document.querySelector('#userLogged')
-  const currentUser = JSON.parse(localStorage.getItem('user'))
-  actualUser.innerHTML = `Welcome ${currentUser}`;
-}
+toasCloseL.addEventListener("click", () => {
+    errorToastL.classList.toggle("show");
+});
 
-if(localStorage.getItem('user') != null){
-  showUser()
-}
-
-toasClose.addEventListener('click', ()=>{
-  errorToast.classList.toggle("show")
-})
-
-
-loginForm.addEventListener('submit', (e)=>{
+loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const loginEmail = document.querySelector('#loginEmail').value
-    const loginPassword = document.querySelector('#loginPassword').value
+    const loginEmail = document.querySelector("#loginEmail").value;
+    const loginPassword = document.querySelector("#loginPassword").value;
 
     auth
         .signInWithEmailAndPassword(loginEmail, loginPassword)
-        .then(userCredential => {
+        .then((userCredential) => {
             //clear form
             loginForm.reset();
             signinModal.hide();
-            localStorage.setItem('user', JSON.stringify(userCredential.user.email)) ;
-            showUser()
-            console.log('Inicio de sesión exitoso')
+            localStorage.setItem("user", JSON.stringify(userCredential.user.email));
+            showUser();
+            console.log("Successful login");
         })
-        .catch((error) => { // Error
+        .catch((error) => {
+            console.log(error)
+            // Error
             switch (error.code) {
-                case 'auth/invalid-email':
-                    errorToast.classList.toggle("show")
-                    errorMsg.innerHTML = 'El formato del correo ingresado no es valido, verifica e intente de nuevo';
-                break;
-                case 'auth/user-not-found':
-                    errorToast.classList.toggle("show")
-                    errorMsg.innerHTML = 'No hay usuario registrado con ese correo, verifica e intente de nuevo';
-                break;
-                case 'auth/wrong-password':
-                    errorToast.classList.toggle("show")
-                    errorMsg.innerHTML = 'La contraseña no es válida, verifica e intente de nuevo';
-                break;
+                case "auth/invalid-email":
+                    errorToastL.classList.toggle("show");
+                    errorMsgL.innerHTML =
+                        "The email format entered is not valid, please check and try again.";
+                    break;
+                case "auth/user-not-found":
+                    errorToastL.classList.toggle("show");
+                    errorMsgL.innerHTML =
+                        "There is no user registered with that email, please check and try again.";
+                    break;
+                case "auth/wrong-password":
+                    errorToastL.classList.toggle("show");
+                    errorMsgL.innerHTML =
+                        "The password is invalid, verify and try again.";
+                    break;
+                case "auth/too-many-requests":
+                    errorToastL.classList.toggle("show");
+                    errorMsgL.innerHTML =
+                        "Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.";
+                    break;
                 default:
-                break;
+                    break;
             }
         });
-})
+});
 
-
-//Logout
-const logoutButton = document.querySelector('#logout');
-
-logoutButton.addEventListener('click', (e)=>{
+logoutButton.addEventListener("click", (e) => {
     e.preventDefault();
-    auth.signOut().then(()=>{
-        console.log('Cerraste sesión de forma exitosa')
-        localStorage.clear();
+    auth.signOut().then(() => {
+        console.log("You have successfully logged out");
+        localStorage.removeItem("user");
         location.reload();
-    })
-})
+    });
+});
 
-// Google Login
-const googleButton = document.querySelector('#googleButton');
-googleButton.addEventListener('click', (e)=>{
+googleButton.addEventListener("click", (e) => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
-        .then(cred => {
-          localStorage.setItem('user', JSON.stringify(cred.user.email)) ;
-          showUser()            
+    auth
+        .signInWithPopup(provider)
+        .then((cred) => {
+            localStorage.setItem("user", JSON.stringify(cred.user.email));
+            showUser();
         })
-        .then(result => {
+        .then((result) => {
             loginForm.reset();
             signinModal.hide();
         })
-        .catch(err => {
-            console.log(err)
-        })
-})
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
-// Eventos
+
 // list for auth state change
-auth.onAuthStateChanged(user => {
-    
-  if(user){
-    loginCheck(user);
-  }else{
-      loginCheck(user);
-  }
-})
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        loginCheck(user);
+    } else {
+        loginCheck(user);
+    }
+});
